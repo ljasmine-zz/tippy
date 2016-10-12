b2570aaebcc62e29f8bfe04af72645cc472d5ee1
Jasmine Lee <jasmine.lee@airbnb.com>
2016-10-12 09:38:05 -0700

    First Draft

---
+++ tippy/SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  tippy
+//
+//  Created by jasmine_lee on 10/11/16.
+//  Copyright © 2016 jasmine_lee. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tippy/Theme.swift
@@ -0,0 +1,9 @@
+//
+//  Theme.swift
+//  tippy
+//
+//  Created by jasmine_lee on 10/11/16.
+//  Copyright © 2016 jasmine_lee. All rights reserved.
+//
+
+import Foundation
